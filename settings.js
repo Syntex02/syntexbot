2ea9f08dca32b5363a0d5f7d3102f84e1af8ce7c
Syntex02 <106860959+Syntex02@users.noreply.github.com>
2022-07-07 10:36:04 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,8 +3,8 @@
 //or copy the codes/script,
 //pls give credit
 //no credit? i will take action immediately
-//© 2022 Xeon Bot Inc. Cheems Bot MD
-//Thank you to Lord Buddha, Family and Myself
+//© ᴀꜱᴡᴀɴᴛʜϟ☻⁩ Inc. Syntax-Bot
+//Thank you to ᴀꜱᴡᴀɴᴛʜϟ☻⁩
 //════════════════════════════//
 //recode kar ke youtube pe upload kar rhe hai ya
 //codes copy kar ke apne script me dal rhe
@@ -54,23 +54,23 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.vcardowner = ['916909137213'] //ur owner number
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
-global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.vcardowner = ['917012498352'] //ur owner number
+global.ownername = "ᴀꜱᴡᴀɴᴛʜϟ☻⁩" //ur owner name
+global.ytname = "YT: ᴀꜱᴡᴀɴᴛʜϟ☻⁩" //ur yt chanel name
+global.socialm = "Instagram: ᴀꜱᴡᴀɴᴛʜϟ☻⁩" //ur github or insta name
+global.location = "India, kerala, kannur" //ur location
 
 //bot bomdy 
-global.owner = ['916909137213']
-global.ownertag = '916909137213' //ur tag number
-global.botname = 'Cheems Bot MD V4' //ur bot name
-global.linkz = "https://youtu.be/WiIqCdiDjFo" //your theme url which will be displayed on whatsapp
-global.websitex = "https://youtu.be/WiIqCdiDjFo" //ur website to be displayed
-global.botscript = 'https://github.com/DGXeon/CheemsBot-MD4' //script link
-global.reactmoji = "😘" //ur menu react emoji
+global.owner = ['917012498352']
+global.ownertag = '917012498352' //ur tag number
+global.botname = 'Syntax-Bot' //ur bot name
+global.linkz = "wa.me/917012498352" //your theme url which will be displayed on whatsapp
+global.websitex = "https://instagram.com/https://instagram.com/ᴀꜱᴡᴀɴᴛʜϟ☻⁩" //ur website to be displayed
+global.botscript = 'https://github.com/Syntex02/syntexbot' //script link
+global.reactmoji = "❤️" //ur menu react emoji
 global.themeemoji = "🦄" //ur theme emoji
-global.packname = "Cheems Bot Stickers\n\n\n\n\n\n\n\n\n\n\n🦄🦄🦄 " //ur sticker watermark packname
-global.author = "🦄Dream Guy Xeon" //ur sticker watermark author
+global.packname = "Syntax-Bot\n\n\n\n\n\n\n\n\n\n\n🦄🦄🦄 " //ur sticker watermark packname
+global.author = "ᴀꜱᴡᴀɴᴛʜϟ☻⁩" //ur sticker watermark author
 
 //Bot theme media
 global.thum = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
@@ -79,7 +79,7 @@ global.err4r = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur error pic
 global.thumb = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
 
 //damtabase
-global.premium = ['916909137213'] //ur premium numbers
+global.premium = ['917012498352'] //ur premium numbers
 global.ntilinkytvid = []
 global.ntilinkytch = []
 global.ntilinkig = []
